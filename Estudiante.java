4f258b3ddb27b3d07e4cce2484cc60ef87b7b5f4
alexnalfaro <93652575+alexnalfaro@users.noreply.github.com>
2022-05-26 12:08:35 +0200

    Add files via upload

--- Estudiante.java
+++ Estudiante.java
@@ -1,51 +1,21 @@
-package EJERCICIO1_;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+package EJERCICIO1;
 
-public class Estudiante {
+public class Estudiante 
+{
 	private String id;
-	private String name;
-	private String city;
-	private String telephone;
-	private String fechaNacmto;
-	private String degreeCode;
+	private String nombre;
+	private String ciudad;
+	private String telefono;
+	private String fecha;
+	private String codgrado;
 	
 	public Estudiante(String i,String n, String c, String t, String f, String d) {
 		this.id=i;
-		this.name=n;
-		this.city=c;
-		this.telephone=t;
-		this.fechaNacmto=f;
-		this.degreeCode=d;
-	}
-	
-//	public Estudiante(String id2, String nombre, String ciudad, String telefono, String fechaNacimiento) {
-//		System.out.println("Dame el ID del estudiante.");
-//		this.id=Console.readString();
-//		System.out.println("Dame el nombre del estudiante.");
-//		this.name=Console.readString();
-//		System.out.println("Dame la ciudad del estudiante.");
-//		this.city=Console.readString();
-//		System.out.println("Dame el telefono del estudiante.");
-//		this.telephone=Console.readString();
-//		System.out.println("Dame la Fecha de nacimiento del estudiante.");
-//		this.fechaNacmto =Console.readString();
-//		System.out.println("Dame el Degree Code del estudiante.");
-//		this.degreeCode=Console.readInt();
-//	}
-	
-	public String getDegreeCode() {
-		return degreeCode;
-	}
-
-	public void setDegreeCode(String degreeCode) {
-		this.degreeCode = degreeCode;
-	}
-
-	public String toString() {
-		return "El alumno tiene como id: "+this.id+"."+"\n"+"Nombre: "+this.name+"."+"\n"+"Ciudad: "+this.city+"."+"\n"+"Teléfono: "+this.telephone+"."+"\n"+"Fecha nacimiento: "+this.fechaNacmto+".";
+		this.nombre=n;
+		this.ciudad=c;
+		this.telefono=t;
+		this.fecha=f;
+		this.codgrado=d;
 	}
 
 	public String getId() {
@@ -56,36 +26,45 @@ public class Estudiante {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getNombre() {
+		return nombre;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
-	public String getCity() {
-		return city;
+	public String getCiudad() {
+		return ciudad;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
 	}
 
-	public String getTelephone() {
-		return telephone;
+	public String getTelefono() {
+		return telefono;
 	}
 
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
 	}
 
-	public String getFechaNacmto() {
-		return fechaNacmto;
+	public String getFecha() {
+		return fecha;
 	}
 
-	public void setFechaNacmto(String fechaNacmto) {
-		this.fechaNacmto = fechaNacmto;
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
 	}
+
+	public String getCodgrado() {
+		return codgrado;
+	}
+
+	public void setCodgrado(String codgrado) {
+		this.codgrado = codgrado;
+	}
+		
 
 }
